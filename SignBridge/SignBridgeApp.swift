c09ee90c148b3d747582a8145afa82e0fa715fc9
5hyam11 <sundararajanshyam1@gmail.com>
2026-02-21 23:36:04 -0800

    Initial Commit

---
+++ SignBridge/SignBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignBridgeApp.swift
+//  SignBridge
+//
+//  Created by Shyam Narayan Sundararajan on 2/21/26.
+//
+
+import SwiftUI
+
+@main
+struct SignBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
